1777b40b18b7fbafc3031c07a219ef720a0e5fde
wlilianhua <wlilianhua@163.com>
2021-11-19 18:14:51 +0800

    add 88

--- src/array/88.js
+++ src/array/88.js
@@ -7,5 +7,23 @@
  * @param {*} n 
  */
 var merge = function(nums1, m, nums2, n) {
+    
+    const result = []
 
+    let i = 0
+    let j = 0
+
+    while(i < m || j < n) {
+        if(j >= n || (i < m && nums1[i] <= nums2[j])) {
+            result.push(nums1[i])
+            i++
+        }else {
+            result.push(nums2[j])
+            j++
+        }
+    }
+
+    for(let t =0;t<m+n;t++) {
+        nums1[t] = result[t]
+    }
 };
